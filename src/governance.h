d894fd0e1d4c324b3cc46d7a48979c2efc85b2ae
schnibble <ilya@dash.org>
2017-07-06 07:40:54 +0300

    fix vulnerability with mapMasternodeOrphanObjects
    The vulnerability is that a malicious node can send a lot of NetMsgType::MNGOVERNANCEOBJECT messages which refer to many arbitrary MN's. In this case, mapMasternodeOrphanObjects will grow unrestrictedly.

--- src/governance.h
+++ src/governance.h
@@ -203,6 +203,8 @@ public: // Types
 
     typedef txout_m_t::const_iterator txout_m_cit;
 
+    typedef std::map<COutPoint, int> txout_int_m_t;
+
     typedef std::set<uint256> hash_s_t;
 
     typedef hash_s_t::iterator hash_s_it;
@@ -241,6 +243,7 @@ private:
     count_m_t mapSeenGovernanceObjects;
 
     object_time_m_t mapMasternodeOrphanObjects;
+    txout_int_m_t mapMasternodeOrphanCounter;
 
     object_m_t mapPostponedObjects;
     hash_s_t setAdditionalRelayObjects;
