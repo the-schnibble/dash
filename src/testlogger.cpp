5c0b9f83162260b88676c66f9a3b5f3a7e1ac957
schnibble <ilya@dash.org>
2017-08-01 16:31:23 +0300

    CTestLogger added

---
+++ src/testlogger.cpp
@@ -0,0 +1,76 @@
+#include "testlogger.h"
+
+/* Example
+
+ Original log invocation:
+   LogPrint("gobject", "CGovernanceManager::UpdateCachesAndClean -- Checking object for deletion: %s, deletion time = %d, time since deletion = %d, delete flag = %d, expired flag = %d\n",
+            strHash, pObj->GetDeletionTime(), nTimeSinceDeletion, pObj->IsSetCachedDelete(), pObj->IsSetExpired());
+
+ mapTestMessages filter entry:
+   {"CGovernanceManager::UpdateCachesAndClean -- Checking object for deletion: %s, deletion time = %d, time since deletion = %d, delete flag = %d, expired flag = %d\n",
+    TestFormat("checking_for_deletion",0,4)}
+
+ output message: checking_for_deletion:77d790e590cb0323d9e77eabb565d3720de5744426c4238d84bb4da3be636aca:1
+   where "77d7....6aca" and "1" are first and fifth parameters, respectively
+
+*/
+
+
+const std::map<std::string, TestFormat> mapTestMessages = {
+
+    // Network messages
+
+    {"PushInventory --  inv: %s peer=%d\n",
+     TestFormat("push_inventory",0)},
+
+    {"SendMessages -- GETDATA -- requesting inv = %s peer=%d\n",
+     TestFormat("get_data",0)},
+
+
+    // Messages related to governance object manager
+
+    {"CNode::AskFor -- WARNING: inventory message dropped: mapAskFor.size = %d, setAskFor.size = %d, MAPASKFOR_MAX_SZ = %d, SETASKFOR_MAX_SZ = %d, nSkipped = %d, peer=%d\n",
+     TestFormat("inv_dropped",0)},
+
+    {"MNGOVERNANCEOBJECT -- Received object: %s\n",
+     TestFormat("govobj_received",0)},
+
+    {"MNGOVERNANCEOBJECT -- Too many orphan objects, missing masternode=%s\n",
+     TestFormat("too_many_orphans",0)},
+
+    {"MNGOVERNANCEOBJECT -- Missing masternode for: %s, strError = %s\n",
+     TestFormat("missing_mn",0)},
+
+    {"CGovernanceManager::MasternodeRateCheck -- Rate too high: object hash = %s, masternode vin = %s, object timestamp = %d, rate = %f, max rate = %f\n",
+     TestFormat("rate_too_high",0)},
+
+    {"CGovernanceManager::MasternodeRateCheck -- object %s rejected due to too old timestamp, masternode vin = %s, timestamp = %d, current time = %d\n",
+     TestFormat("govobj_invalid_time",0)},
+
+    {"CGovernanceManager::MasternodeRateCheck -- object %s rejected due to too new (future) timestamp, masternode vin = %s, timestamp = %d, current time = %d\n",
+     TestFormat("govobj_invalid_time",0)},
+
+    {"MNGOVERNANCEOBJECT -- Received unrequested object: %s\n",
+     TestFormat("govobj_unrequested_received",0)},
+
+    {"MNGOVERNANCEOBJECT -- Received already seen object: %s\n",
+     TestFormat("govobj_seen_received",0)},
+
+    {"MNGOVERNANCEOBJECT -- Governance object is invalid - %s\n",
+     TestFormat("govobj_invalid_received",0)},
+
+    {"MNGOVERNANCEOBJECT -- Not enough fee confirmations for: %s, strError = %s\n",
+     TestFormat("govobj_missing_confs",0)},
+
+    {"AddGovernanceObject -- %s new, received form %s\n",
+     TestFormat("govobj_accepted",0)}
+};
+
+CTestLogger& CTestLogger::GetInstance()
+{
+    static CTestLogger* testlogger = new CTestLogger(mapTestMessages);
+    return *testlogger;
+
+    //static CTestLogger testlogger(mapTestMessages);
+    //return testlogger;
+}
---
+++ src/testlogger.h
@@ -0,0 +1,114 @@
+#ifndef TESTLOGGER_H
+#define TESTLOGGER_H
+
+#include <cassert>
+#include <tuple>
+#include <mutex>
+#include <map>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+template <size_t I>
+struct getarg_impl
+{
+    template <typename T>
+    static std::string get(size_t idx, const T& tup)
+    {
+        std::ostringstream res;
+
+        if (idx == I-1)
+            res << std::get<I-1>(tup);
+        else
+            res << getarg_impl<I-1>::get(idx, tup);
+        return res.str();
+    }
+};
+
+template <>
+struct getarg_impl<0>
+{
+    template <typename T>
+    static std::string get(size_t idx, const T& tup) { assert(false); }
+};
+
+template <typename... Args>
+std::string GetArgByIndex(size_t idx, const Args&... args)
+{
+    return getarg_impl<sizeof...(Args)>::get(idx, std::forward_as_tuple(args...));
+}
+
+
+struct TestFormat
+{
+    template<typename... T>
+    TestFormat(const char* _event_name, T... _indices) : event_name(_event_name), params{_indices...} {}
+
+    const char* event_name;
+    std::vector<int> params;
+};
+
+class CTestLogger
+{
+    const std::map<std::string, TestFormat>& mapMessages;
+
+    int fdLog;
+    std::string pipeName;
+    std::mutex mutex;
+
+public:
+    CTestLogger(const std::map<std::string, TestFormat>& _mapTestMessages) : mapMessages(_mapTestMessages)
+    {
+        fdLog = -1;
+    }
+
+    ~CTestLogger()
+    {
+        if (fdLog != -1)
+            close(fdLog);
+    }
+
+    void Initialize(std::string _pipeName)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        pipeName = _pipeName;
+    }
+
+    static CTestLogger& GetInstance();
+
+    template<typename... Args>
+    void SendMsg(const char* format, const Args&... args)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+
+        if (fdLog == -1 && !pipeName.empty())
+            fdLog = open(pipeName.c_str(), O_WRONLY | O_NONBLOCK);
+
+        if (fdLog == -1)
+            return;
+
+        auto it = mapMessages.find(format);
+        if (it != mapMessages.end())
+        {
+            std::ostringstream msg;
+
+            msg << it->second.event_name;
+            for(const auto& i : it->second.params)
+                msg << ":" << GetArgByIndex(i, args...);
+            msg << "\n";
+
+            if (write(fdLog, msg.str().c_str(), msg.str().length()) == -1) {
+                close(fdLog);
+                fdLog = -1;
+            }
+        }
+    }
+
+};
+
+
+#endif // TESTLOGGER_H
