893bdb7004c1b1fd7c3ffd5f63c9498389e4cd42
schnibble <ilya@dash.org>
2017-09-09 11:55:12 +0300

    remove `#include "chainparams.h"` from netaddress.cpp

--- src/netaddress.cpp
+++ src/netaddress.cpp
@@ -11,7 +11,6 @@
 #include "hash.h"
 #include "utilstrencodings.h"
 #include "tinyformat.h"
-#include "chainparams.h"
 
 static const unsigned char pchIPv4[12] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff };
 static const unsigned char pchOnionCat[] = {0xFD,0x87,0xD8,0x7E,0xEB,0x43};
