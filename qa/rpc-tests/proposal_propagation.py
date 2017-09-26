6b87f0223a5622c944c580ad1cf3c566029a513e
schnibble <ilya@dash.org>
2017-09-26 19:52:25 +0300

    code refactoring

--- qa/rpc-tests/proposal_propagation.py
+++ qa/rpc-tests/proposal_propagation.py
@@ -13,7 +13,6 @@ import time
 
 import testtools
 from models import Proposal
-from dashd import DashDaemon
 
 def now():
     return int(time.time())
@@ -44,35 +43,36 @@ class ProposalPropagationTest(BitcoinTestFramework):
             for j in range(i+1,4):
                 connect_nodes(self.nodes[i], j)
 
-        get_rpc_proxy(rpc_url(0), 0).generate(1)
+        #get_rpc_proxy(rpc_url(0), 0).generate(1)
+        self.nodes[0].generate(1)
         
-    def get_confirmations(self, dashd, txid):
+    def get_confirmations(self, node, txid):
         try:
-            r = dashd.rpc_command("getrawtransaction", txid, 1)
+            r = node.getrawtransaction(txid, 1)
         except:
             return 0
         if r.get('confirmations') is None:
             return 0
         return int(r.get('confirmations'))
 
-    def wait_confirmations(self, dashd, loglistener, txid, count):
+    def wait_confirmations(self, node, loglistener, txid, count):
         while True:
-            confirmations = self.get_confirmations(dashd, txid)
+            confirmations = self.get_confirmations(node, txid)
             if confirmations >= count:
                 break
 
-            block_height = int(dashd.rpc_command("getblockcount")) + count - confirmations
+            block_height = int(node.getblockcount()) + count - confirmations
             #generate required number of blocks
-            dashd.rpc_command('generate', count - confirmations)
+            node.generate(count - confirmations)
             loglistener.expect('update_block_tip:{0}'.format(block_height), 1, 5)
 
     def run_test(self):
 
-        #node1 should be a node started with "-govtest" parameter
-        node1 = DashDaemon(host = '127.0.0.1', user='rt', password = 'rt', port = rpc_port(0))
-        #node2 and node3 are regular nodes
-        node2 = DashDaemon(host = '127.0.0.1', user='rt', password = 'rt', port = rpc_port(1))
-        node3 = DashDaemon(host = '127.0.0.1', user='rt', password = 'rt', port = rpc_port(2))
+#        #node1 should be a node started with "-govtest" parameter
+#        node1 = DashDaemon(host = '127.0.0.1', user='rt', password = 'rt', port = rpc_port(0))
+#        #node2 and node3 are regular nodes
+#        node2 = DashDaemon(host = '127.0.0.1', user='rt', password = 'rt', port = rpc_port(1))
+#        node3 = DashDaemon(host = '127.0.0.1', user='rt', password = 'rt', port = rpc_port(2))
         
         #~ node1 = get_rpc_proxy(rpc_url(0), 0)
         #~ node2 = get_rpc_proxy(rpc_url(1), 1)
@@ -83,7 +83,7 @@ class ProposalPropagationTest(BitcoinTestFramework):
         log3 = testtools.LogListener('/tmp/node2', 10)
 
 
-        while(not node1.is_synced() or not node2.is_synced() or not node3.is_synced()):
+        while(not self.nodes[0].is_synced() or not self.nodes[1].is_synced() or not self.nodes[2].is_synced()):
             print('not yet synced, sleep 5 sec')
             time.sleep(5)
 
@@ -93,11 +93,11 @@ class ProposalPropagationTest(BitcoinTestFramework):
         payout_month = 50
 
         curunixtime = now()
-        payout_address = node1.rpc_command("getnewaddress")
-        proposalfee = node1.proposalfee()
-        superblockcycle = node1.superblockcycle()
-        nextsuperblock = node1.next_superblock_height()
-        curblock = node1.last_superblock_height()
+        payout_address = self.nodes[0].getnewaddress()
+        proposalfee = self.nodes[0].proposalfee()
+        superblockcycle = self.nodes[0].superblockcycle()
+        nextsuperblock = self.nodes[0].next_superblock_height()
+        curblock = self.nodes[0].last_superblock_height()
 
         if nextsuperblock - curblock > 10:
             start_epoch = curunixtime
@@ -119,22 +119,22 @@ class ProposalPropagationTest(BitcoinTestFramework):
         # should be permanently rejected by all nodes
 
         cmd = ['gobject', 'prepare', '0', '1', str(curunixtime), proposal.dashd_serialise()]
-        tx_hash0 = node1.rpc_command(*cmd)
+        tx_hash0 = self.nodes[0].rpc_command(*cmd)
         print 'collateral tx hash: {0}\n'.format(tx_hash0)
         log1.expect_minimum('push_inventory:tx {0}'.format(tx_hash0), 1)
         cmd = ['gobject', 'submit', '0', '1', str(curunixtime), proposal.dashd_serialise(), tx_hash0]
 
         #try to submit by regular node
         try:
-            node2.rpc_command(*cmd)
+            self.nodes[1].rpc_command(*cmd)
         except:
             pass
         else:
-            if self.get_confirmations(node2, tx_hash0) < 1:
+            if self.get_confirmations(self.nodes[1], tx_hash0) < 1:
                 assert False, "TEST FAILED: proposal submitted with zero confirmations"
 
         #submit by special test node
-        object_hash0 = node1.rpc_command(*cmd)
+        object_hash0 = self.nodes[0].rpc_command(*cmd)
         print '\nproposal hash: {0}\n'.format(object_hash0)
 
         log1.expect_minimum('push_inventory:govobj {0}'.format(object_hash0), 1)
@@ -142,7 +142,7 @@ class ProposalPropagationTest(BitcoinTestFramework):
         log3.expect('govobj_received:{0}'.format(object_hash0), 1)
 
         #should be always true in regtest mode
-        if self.get_confirmations(node2, tx_hash0) < 1 and self.get_confirmations(node3, tx_hash0) < 1:
+        if self.get_confirmations(self.nodes[1], tx_hash0) < 1 and self.get_confirmations(self.nodes[2], tx_hash0) < 1:
             log2.expect_maximum('govobj_accepted:{0}'.format(object_hash0), 0, 10)
             log3.expect_maximum('govobj_accepted:{0}'.format(object_hash0), 0, 0)
             log2.expect_maximum('govobj_missing_confs:{0}'.format(object_hash0), 0, 0)
@@ -154,18 +154,18 @@ class ProposalPropagationTest(BitcoinTestFramework):
         proposal.name = 'proposal_'+str(curblock+1)
         proposal.url = 'https://dashcentral.com/' + proposal.name + '_' + str(curunixtime)
 
-        tx_hash = node2.rpc_command(*proposal.get_prepare_command())
+        tx_hash = self.nodes[1].rpc_command(*proposal.get_prepare_command())
         print 'new collateral tx hash: {0}\n'.format(tx_hash)
         log2.expect_minimum('push_inventory:tx {0}'.format(tx_hash), 1)
 
         print '\nwaiting for 1 confirmation\n'
-        self.wait_confirmations(node2, log2, tx_hash, 1)
+        self.wait_confirmations(self.nodes[1], log2, tx_hash, 1)
 
         print '\nsubmit proposal with 1 confirmation'
         # should be pre-accepted by node3 and automatically rebroadcasted when 6 confirmations have passed and then accepted by all nodes
 
         cmd = ['gobject', 'submit', '0', '1', str(curunixtime), proposal.dashd_serialise(), tx_hash]
-        object_hash = node2.rpc_command(*cmd)
+        object_hash = self.nodes[1].rpc_command(*cmd)
         print 'new proposal hash: {0}\n'.format(object_hash)
 
         log2.expect_minimum('push_inventory:govobj {0}'.format(object_hash), 1)
@@ -175,7 +175,7 @@ class ProposalPropagationTest(BitcoinTestFramework):
         log3.expect_maximum('push_inventory:govobj {0}'.format(object_hash), 0, 0)
 
         print '\nwaiting for 6 confirmations\n'
-        self.wait_confirmations(node1, log1, tx_hash, 6)
+        self.wait_confirmations(self.nodes[0], log1, tx_hash, 6)
 
         log2.expect('govobj_accepted:{0}'.format(object_hash), 1)
         log3.expect('govobj_accepted:{0}'.format(object_hash), 1)
